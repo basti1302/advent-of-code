ffd8e1359da31f4ab53c443a07883f38838d3e27
Bastian Krol <bastian.krol@dash0.com>
2025-12-01 14:53:34 +0100

    advent of code 2025: day 1

---
+++ 2025/01/01/index.js
@@ -0,0 +1,40 @@
+import { readFile } from 'node:fs/promises';
+import { EOL } from 'node:os';
+
+var zeroPositions = 0;
+
+try {
+  const contents = await readFile('input.txt', { encoding: 'utf8' });
+  const lines = contents.split(EOL);
+
+  var position = 50;
+  lines.forEach(line => {
+    if (line == null || line.trim() == '') {
+      return;
+    }
+    var sign = 1;
+    const direction = line[0];
+    if (direction == 'R') {
+      // no-op
+    } else if (direction == 'L') {
+      sign = -1;
+    } else {
+      throw new Error(`invalid direction: ${direction}`);
+    }
+    const steps = line.substring(1);
+    const previousPosition = position;
+    position = mod(position + sign * steps, 100);
+    if (position === 0) {
+      console.log(line, sign, steps, previousPosition, position);
+      zeroPositions++;
+    }
+  });
+} catch (err) {
+  console.error(err.message);
+}
+
+console.log(`Zero positions: ${zeroPositions}`);
+
+function mod(a, n) {
+  return ((a % n) + n) % n;
+}
---
+++ 2025/01/02/index.js
@@ -0,0 +1,74 @@
+import { readFile } from 'node:fs/promises';
+import { EOL } from 'node:os';
+
+try {
+  const contents = await readFile('input.txt', { encoding: 'utf8' });
+  const lines = contents.split(EOL);
+
+  var index = 0;
+  var zeroPositions = 0;
+  var position = 50;
+  lines.forEach(line => {
+    if (line == null || line.trim() == '') {
+      return;
+    }
+
+    line = line.split('#')[0].trim();
+
+    var sign = 1;
+    const direction = line[0];
+    if (direction == 'R') {
+      // no-op
+    } else if (direction == 'L') {
+      sign = -1;
+    } else {
+      throw new Error(`invalid direction: ${direction}`);
+    }
+    const previousPosition = position;
+    const previousZeroPositions = zeroPositions;
+
+    const steps = line.substring(1);
+    const fullRotations = Math.floor(steps / 100);
+    const remainingSteps = steps - fullRotations * 100;
+    const positionNoMod = position + sign * remainingSteps;
+    position = mod(positionNoMod, 100);
+
+    // each full rotation of the cylinder adds exactly one intermittent zero position
+    zeroPositions += fullRotations;
+    // if (fullRotations > 0) {
+    //   console.log(
+    //     `${index}: ${line}: multiple full rotations: <->: ${sign}, o: ${fullRotations}, #: ${remainingSteps}, ${previousPosition}->${position}, #0: ${previousZeroPositions} -> ${zeroPositions}`,
+    //   );
+    // }
+
+    // account for turns where we end exactly on 0, not covered by the previous case
+    if (position === 0 && remainingSteps !== 0) {
+      zeroPositions++;
+      // console.log(
+      //   `${index}: ${line}: ending on 0: <->: ${sign}, #: ${steps}, ${previousPosition}->${position}, #0: ${previousZeroPositions} -> ${zeroPositions}`,
+      // );
+    }
+
+    // account for things like going from 5 to 95 via L10
+    if (
+      position !== 0 &&
+      previousPosition !== 0 && //
+      positionNoMod !== position
+    ) {
+      zeroPositions++;
+      //  console.log(
+      //    `${index}: ${line}: <->: crossing 0 with remaining steps: ${sign}, o: ${fullRotations}, #: ${remainingSteps}, ${previousPosition}->${position}, #0: ${previousZeroPositions} -> ${zeroPositions}`,
+      //  );
+    }
+
+    index++;
+  });
+} catch (err) {
+  console.error(err.message);
+}
+
+console.log(`Zero positions: ${zeroPositions}, ending in ${position}`);
+
+function mod(a, n) {
+  return ((a % n) + n) % n;
+}
